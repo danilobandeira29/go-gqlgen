65dbf44177dcd2f317ca75bc37d2408ba94ac43e
Danilo Bandeira <danilo.bandeira@growthtech.com.br>
2022-07-21 08:48:31 -0300

    feat: Mutation: CreateCourse

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -24,7 +24,21 @@ func (r *mutationResolver) CreateCategory(ctx context.Context, input model.NewCa
 
 // CreateCourse is the resolver for the createCourse field.
 func (r *mutationResolver) CreateCourse(ctx context.Context, input model.NewCourse) (*model.Course, error) {
-	panic(fmt.Errorf("not implemented"))
+	var category *model.Category
+	for _, c := range r.Categories {
+		if c.ID == input.CategoryID {
+			category = c
+			break
+		}
+	}
+	course := &model.Course{
+		ID:          fmt.Sprintf("T%d", rand.Int()),
+		Name:        input.Name,
+		Description: input.Description,
+		Category:    category,
+	}
+	r.Courses = append(r.Courses, course)
+	return course, nil
 }
 
 // CreateChapter is the resolver for the createChapter field.
